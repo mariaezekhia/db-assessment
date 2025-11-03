6ac5dd9a1a9aee61835539334459b2a00ee76cb6
mariaezekhia <mariagulipatan@riccarton.school.nz>
2025-11-04 09:19:36 +1300

    resubbbb

--- kdrama.py
+++ kdrama.py
@@ -22,8 +22,6 @@ def print_query(view_name:str):
     print(tabulate(results,headings))
     db.close()
 
-print_query ('season 2')
-
 menu_choice ='' 
 while menu_choice != 'Z':
     menu_choice = input('Welcome to the Kdrama database\n\n'
@@ -32,6 +30,14 @@ while menu_choice != 'Z':
                         'B: Kdrama release after 2018\n'
                         'C: Kdrama with the ratings of below 4.5\n'
                         'D: Kdrama with has season 2\n'
+                        'E: Kdrama with the ratings of five\n'
+                        'F: Kdrama that you can watch on viki \n'
+                        'G: Kdrama that you can watch on netflix\n'
+                        'H: Kdrama that has been directed by lee eun bok\n'
+                        'I: kdrama available both netflix and viki\n'
+                        'J: Kdrama release in 2017 with 16 episode and has 4.9 ratings\n'
+                        'K: kdrama with 16 episode\n'
+                        'L: Kdrama release in 2016\n'
                         'Z: Exit\n\nType option here: ')
     menu_choice = menu_choice.upper()
     if menu_choice == 'A':
@@ -42,4 +48,19 @@ while menu_choice != 'Z':
         print_query('below 4.5')
     elif menu_choice == 'D':
         print_query('season 2')
-    
+    elif menu_choice == 'E':
+        print_query('five')
+    elif menu_choice == 'F':
+        print_query('viki')
+    elif menu_choice == 'G':
+        print_query('netflix')
+    elif menu_choice == 'H': 
+        print_query('director')
+    elif menu_choice == 'I':
+        print_query('both')
+    elif menu_choice == 'J':
+        print_query('AND')
+    elif menu_choice == 'K':
+        print_query('16 episode')
+    elif menu_choice == 'L':
+        print_query('2016')
